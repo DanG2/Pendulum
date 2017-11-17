0316d5cc88c4e3a33d2152da2f7db956e23ac130
DanG2 <danielgankov@yahoo.com>
2017-11-17 12:45:50 -0600

    Pendulum Project

---
+++ src/Palindrome.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        String inputWord = new String(JOptionPane.showInputDialog("What is the word you would like to enter"));
+        getInput(inputWord);
+    }
+    public static String getInput(String iw){
+        String
+    }
+}
